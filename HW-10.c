085981c509703ed5405546a245778641917069aa
Luka <luka.toni93@gmail.com>
2014-06-21 18:47:59 +0200

    Update HW-10.c

--- HW-10.c
+++ HW-10.c
@@ -3,14 +3,7 @@
 
 int maxValue(int a,int b)
 {
-    if(a > b)
-    {
-        return a;
-    }
-    else
-    {
-        return b;
-    }
+    return (a > b) ? a : b ;
 }
 
 void Knapsack(int items,int maxWeight, int *weight,int *value)
@@ -19,54 +12,27 @@ void Knapsack(int items,int maxWeight, int *weight,int *value)
         int j;
         int w;
 
-        /*2 dimenzionalna tabela*/
+        /*2 dimensional table*/
         int **dp = malloc(sizeof(int*)*(items+1));
-        /*nastavimo drugo diminzionalao*/
+
         for(i = 0; i < items + 1 ; i++)
-        {
             dp[i] = malloc(sizeof(int)*(items+1));
-        }
-
-        /* dp[i][w] represents maximum value that can be attained if the maximum weight is w and
-           items are chosen from 1...i */
-        /* dp[0][w] = 0 for all w because we have chosen 0 items */
-
-        /* dp[0][j]  vse nastavimo na 0 */
 
         for(j = 0 ; j <= maxWeight ; j++)
-        {
                 dp[0][j] = 0;
-        }
 
-        /* dp[j][0]  vse nastavimo na 0 */
+        /* dp[j][0]  set on 0 */
         for(j = 0; j <= items ; j++)
-        {
                 dp[j][0] = 0;
-        }
-/*
-123456789
-123456000 4096
-12345678 64
-1024 134217728
-12345678 96
-999 999999
-*/
-
 
         for(j = 1; j < items + 1 ; j++)
         {
                 for(w = 0 ; w < maxWeight + 1 ; w++)
                 {
-
-
-                         dp[j][w] = dp[j-1][w]; /* ne vzamemo */
+                        dp[j][w] = dp[j-1][w]; /* ne vzamemo */
 
                         if(w-weight[j] >= 0)
-                        {
-                                /* vzamemo */
                                 dp[j][w] = maxValue( dp[j][w] , dp[j-1][w - weight[j]] + value[j] );
-                        }
-
                 }
         }
         printf("%d\n", dp[items][maxWeight]);
@@ -74,62 +40,31 @@ void Knapsack(int items,int maxWeight, int *weight,int *value)
 
 int main()
 {
-    //prostor za podatke
     int maxWeight;
     scanf("%d",&maxWeight);
     if(maxWeight < 1 || 1000000000 < maxWeight)
-    {
-        return 0; /*napacen podatek*/
-    }
+        return 0; /*error*/
 
     int *weight = malloc(sizeof(int)*41);
     int *value = malloc(sizeof(int)*41);
 
     int items = 0;
-    /*dokler sta 2 elementa v vrstice se items povecuje za ena*/
-    while(scanf("%d%d",&weight[items+1],&value[items+1]) == 2)
+  
+    while(scanf("%d%d", &weight[items+1] , &value[items+1] ) == 2)
     {
         /* 1 < weight[items] < 1000000 */
         if(weight[items] < 1 || 1000000000 < weight[items])
-        {
-            return 0; /*napacen podatek*/
-        }
+            return 0; /*error*/
+            
        /* 1 < value[items] < 1000000 */
         if(value[items] < 1 || 1000000000 < value[items])
-        {
-            return 0; /*napacen podatek*/
-        }
-        /*pristejemo en popoln podatek*/
+            return 0; /*error*/
+            
+        /*add item*/
         items++;
     }
-//    int i;
-//    for (i = 0 ; i < items ; i++)
-//        printf("%d %d\n", weight[i],value[i] );
+    
+    Knapsack(items,maxWeight,weight,value);
 
-    //printf("ITEMS %d\n", items );
-
-//    if(items == 0)
-//    {
-//        printf("0\n");
-//    }
-//
-//
-//    else if(items == 1)
-//    {
-//        if(weight[0] < maxWeight)
-//        {
-//            printf("%d\n", value[0]);
-//        }
-//        else
-//        {
-//            printf("0\n");
-//        }
-//    }
-
-//    else
-//    {
-        //items++;
-        Knapsack(items,maxWeight,weight,value);
-//    }
     return 0;
 }
